a4a66a2c65d666eb3ec3d340d42c1c57698d84f4
Nico Haase <nico@nicohaase.de>
2017-06-24 18:51:02 +0200

    Refreshing dependencies

--- FacebookPostingHelper/FacebookPostingHelper.php
+++ FacebookPostingHelper/FacebookPostingHelper.php
@@ -1,32 +1,31 @@
 <?php
 namespace FacebookPostingHelper;
 
+use Facebook\Exceptions\FacebookSDKException;
+use Facebook\Facebook;
 use Piwik\Ini\IniReader;
 use Piwik\Ini\IniWriter;
-use Facebook\FacebookSession;
-use Facebook\FacebookRedirectLoginHelper;
-use Facebook\FacebookRequest;
 
 class FacebookPostingHelper
 {
 
     /**
-     *
      * @var string
      */
     private $configFilename;
 
     /**
-     *
-     * @var string[]
+     * @var string[][]
      */
     private $config;
 
+    /**
+     * @param string $fileName
+     */
     public function __construct($fileName)
     {
         $this->configFilename = $fileName;
         $this->readConfig();
-        FacebookSession::setDefaultApplication($this->getAppId(), $this->getAppSecret());
     }
 
     public function readConfig()
@@ -41,81 +40,123 @@ class FacebookPostingHelper
         $writer->writeToFile($this->configFilename, $this->config);
     }
 
+    /**
+     * @param string $section
+     * @param string $key
+     * @param bool $showError
+     * @return string
+     * @throws \Exception
+     */
     private function getFromConfig($section, $key, $showError = false)
     {
         if(isset($this->config[$section]) && isset($this->config[$section][$key])) {
             return $this->config[$section][$key];
         }
         if($showError) {
-            throw new Exception(sprintf('Missing key %s in section %s', $key, $section));
+            throw new \Exception(sprintf('Missing key %s in section %s', $key, $section));
         }
     }
 
+    /**
+     * @return string
+     */
     public function getAppId()
     {
         return $this->getFromConfig('appDetails', 'appId', true);
     }
 
+    /**
+     * @return string
+     */
     public function getAppSecret()
     {
         return $this->getFromConfig('appDetails', 'appSecret', true);
     }
 
+    /**
+     * @return string
+     */
     public function getPageAccessToken()
     {
         return $this->getFromConfig('authorization', 'pageAccessToken');
     }
 
+    /**
+     * @return string
+     */
     public function getPageId()
     {
         return $this->getFromConfig('pageDetails', 'pageId', true);
     }
 
+    /**
+     * @return Facebook
+     */
     public function getAppSession()
     {
-        return FacebookSession::newAppSession();
+        return new Facebook([
+            'app_id'     => $this->getAppId(),
+            'app_secret' => $this->getAppSecret(),
+            'default_graph_version' => 'v2.9',
+        ]);
     }
 
+    /**
+     * @return bool
+     */
     public function loginValid()
     {
         $pageAccessToken = $this->getPageAccessToken();
-        if(is_null($pageAccessToken) === true) {
+        if (is_null($pageAccessToken) === true) {
             return false;
         }
-        
+
         $appSession = $this->getAppSession();
-        $request = new FacebookRequest($appSession, 'GET', '/debug_token', array(
-            'input_token' => $pageAccessToken
-        ));
-        $data = $request->execute()
-            ->getGraphObject()
-            ->asArray();
-        $isValid = $data['is_valid'];
-        
-        return $isValid;
+        $appSession->setDefaultAccessToken($pageAccessToken);
+
+        try {
+            $request = $appSession->get('/debug_token');
+        } catch (FacebookSDKException $exception) {
+            return false;
+        }
+
+        return $request->getGraphNode()->getField('is_valid');
     }
 
-    public function post($postMessage, $link = null, $caption = null)
+    /**
+     * @param string $postMessage
+     * @param string $link
+     * @param string $caption
+     * @param string $linkName
+     * @param string $imageUrl
+     * @return string
+     */
+    public function post($postMessage, $link = null, $caption = null, $linkName = null, $imageUrl = null)
     {
         $appSession = $this->getAppSession();
-        $postData = array(
-            'access_token' => $this->getPageAccessToken(),
-            'message' => $postMessage
-        );
+        $appSession->setDefaultAccessToken($this->getPageAccessToken());
+        $postData = array('message' => $postMessage);
 
-        if(is_null($link) === false) {
+        if (is_null($link) === false) {
             $postData['link'] = $link;
         }
 
-        if(is_null($caption) === false) {
+        if (is_null($linkName) === false) {
+            $postData['name'] = $linkName;
+        }
+
+        if (is_null($caption) === false) {
             $postData['caption'] = $caption;
         }
 
-        $request = new FacebookRequest($appSession, 'POST', '/' . $this->getPageId() . '/feed', $postData);
-        $page_post = $request->execute()
-            ->getGraphObject()
-            ->asArray();
-        return $page_post['id'];
+        if (is_null($imageUrl) === false) {
+            $postData['picture'] = $imageUrl;
+        }
+
+        $postUrl = '/' . $this->getPageId() . '/feed';
+
+        $response = $appSession->post($postUrl, $postData);
+        return $response->getGraphNode()->getField('id');
     }
 
     public function performLogin()
@@ -124,41 +165,29 @@ class FacebookPostingHelper
         $server = $_SERVER['HTTP_HOST'];
         $relativePath = $_SERVER['PHP_SELF'];
         $myUrl = $protocol . $server . $relativePath;
-        
+
         session_start();
-        $helper = new FacebookRedirectLoginHelper($myUrl);
-        try {
-            $session = $helper->getSessionFromRedirect();
-        } catch (\Exception $ex) {}
-        
-        if (isset($session)) {
-            $userAccessToken = $session->getToken();
-            $pageAccessToken = $this->getPageAccessTokenFromUserAccessToken($userAccessToken);
-            if(!isset($this->config['authorization']) || !is_array($this->config['authorization'])) {
+        $facebookSession = $this->getAppSession();
+        $helper = $facebookSession->getRedirectLoginHelper();
+
+        $userAccessToken = $helper->getAccessToken();
+
+        if ($userAccessToken) {
+            $oauth2Client = $facebookSession->getOAuth2Client();
+            $pageAccessToken = $oauth2Client->getLongLivedAccessToken($userAccessToken);
+            if (!isset($this->config['authorization']) || !is_array($this->config['authorization'])) {
                 $this->config['authorization'] = array();
             }
-            $this->config['authorization']['pageAccessToken'] = $pageAccessToken;
+            $this->config['authorization']['pageAccessToken'] = $pageAccessToken->getValue();
             $this->writeConfig();
             return true;
         } else {
-            header('Location: ' . $helper->getLoginUrl(array(
-                'public_profile',
-                'manage_pages',
-                'publish_pages'
-            )));
+            header('Location: ' . $helper->getLoginUrl($myUrl, array(
+                    'public_profile',
+                    'manage_pages',
+                    'publish_pages'
+                )));
             return false;
         }
-    }
-
-    public function getPageAccessTokenFromUserAccessToken($userAccessToken)
-    {
-        $userSession = new FacebookSession($userAccessToken);
-        $request = new FacebookRequest($userSession, 'GET', '/' . $this->getPageId(), array(
-            'fields' => 'access_token'
-        ));
-        $access_token = $request->execute()
-            ->getGraphObject()
-            ->asArray();
-        return $access_token['access_token'];
     }
 }
